230059d36b9a88439061756886d4a9a01b9fbc0c
temasaur <temasaur@gmail.com>
2022-10-29 12:26:35 +0500

    add display of post previews

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import {getPosts} from './data.js';
+import {showPreviews} from './photos/previews.js';
+
+
+showPreviews(getPosts());
---
+++ js/templates/template_engine.js
@@ -0,0 +1,43 @@
+/**
+ * Класс, предоставляющий функциноал шаблонизатора, где шаблон представляет HTML-строку
+ * с параметрами, обернутыми в `{{фигурные скобки}}`. Сложный параметр разделяется
+ * точкой `{{object.property}}`.
+ */
+class TemplateEngine {
+  /**
+   * @param {String} template Строка-шаблон
+   */
+  constructor(template) {
+    this.template = template;
+  }
+
+  /**
+   * Подставляет значения в создынный шаблон
+   * @param {object} data объект с данными для подставления
+   * @returns {String} HTML-строка с подставленными значениями
+   */
+  compile(data) {
+    const curlyBracesRe = /{{(.+)}}/g;
+    return this.template.replace(curlyBracesRe, this._replaceMatch(data));
+  }
+
+  /**
+   * Возвращает callback `replacer` для использования в `String.prototype.replace(pattern, replacer)`
+   * @param {object} data объект с данными для подставления
+   * @returns {function(String):String} `replacer` возвращает по `match` значение одноименного параметра в `data`
+   */
+  _replaceMatch(data) {
+    const replacer = (match) => {
+      const keyName = match.match(/{{(.+)}}/)[1].trim();
+      const property = keyName.match(/(.+)\.(.+)/);
+
+      return property
+        ? data[property[1]][property[2]]
+        : data[keyName];
+    };
+
+    return replacer;
+  }
+}
+
+export {TemplateEngine};
